497c4df15f8d1bccdd6fee13d95e159b32b56c78
Yoav Brand <yoavbrand@MacBook-Air.local>
2021-08-04 22:48:40 +0300

    some changes

--- type_i_func.c
+++ type_i_func.c
@@ -1,9 +1,23 @@
+/* 
+*I type Instructions.
+* Math and Logic Instructions: addi, subi, andi, ori, nori.
+* Copy Instructions: move, mvhi, mvlo.
+* Each Instruction has 3 operands and 3 registers
+* The Instructions is being done between the "rs" and the "rt" registers, and the resualt is being saved in the "rd" register.
+* All of the Instructions have the same code, opcode = 0 (include nothing).
+* Each Instruction has a uniqe name field, the "funct" field, which is uniqe for each Instruction.
+*/
+
 #include "assembler.h"
 #include "param.h"
 #include "func.h"
 
-
-void addi_func(I *instruction) {\
+/* 
+*This function adds the value of the immediate value ('inned')
+* to the value in register ('rs') and save the action resault in 'rt'
+*/
+void addi_func(I *instruction)
+{
     unsigned int rs_index = instruction->rs;
     unsigned int immed = instruction->immed;
     unsigned int rs_val = (regArray[rs_index]->val);
@@ -11,51 +25,79 @@ void addi_func(I *instruction) {\
     (regArray[instruction->rt]->val) = res;
 }
 
-void subi_func(I* instruction) {
+/*  This function subtrac the value of the immediate value from the value in 'rs' */
+
+void subi_func(I *instruction)
+{
     unsigned int rs_index = instruction->rs;
     unsigned int immed = instruction->immed;
     unsigned int rs_val = (regArray[rs_index]->val);
     unsigned int res = immed - rs_val;
     (regArray[instruction->rt]->val) = res;
 }
-void andi_func(I* instruction) {
+
+/*  This function simulate the "and" (&) action between the immediate value and the value in 'rs' */
+
+void andi_func(I *instruction)
+{
     unsigned int rs_index = instruction->rs;
     unsigned int immed = instruction->immed;
     unsigned int rs_val = (regArray[rs_index]->val);
     unsigned int res = immed & rs_val;
     (regArray[instruction->rt]->val) = res;
 }
-void ori_func(I* instruction) {
+
+/*  This function simulate the "or" (||) action between the immediate value and the value in 'rs' */
+
+void ori_func(I *instruction)
+{
     unsigned int rs_index = instruction->rs;
     unsigned int immed = instruction->immed;
     unsigned int rs_val = (regArray[rs_index]->val);
     unsigned int res = immed | rs_val;
     (regArray[instruction->rt]->val) = res;
 }
-void nori_func(I* instruction) {
+
+/*  This function simulate the "nor" (!(a|| b)) action between the immediate value and the value in 'rs' */
+
+void nori_func(I *instruction)
+{
     unsigned int rs_index = instruction->rs;
     unsigned int immed = instruction->immed;
     unsigned int rs_val = (regArray[rs_index]->val);
     unsigned int res = ~(immed | rs_val);
     (regArray[instruction->rt]->val) = res;
 }
-void bne_func(I* instruction) {
+
+/*  This function simulate the "and" (&) action between the immediate value and the value in 'rs' */
+
+void bne_func(I *instruction)
+{
 }
-void beq_func(I* instruction) {
+void beq_func(I *instruction)
+{
 }
-void blt_func(I* instruction) {
+void blt_func(I *instruction)
+{
 }
-void bgt_func(I* instruction) {
+void bgt_func(I *instruction)
+{
 }
-void lb_func(I* instruction) {
+void lb_func(I *instruction)
+{
 }
-void sb_func(I* instruction) {
+void sb_func(I *instruction)
+{
 }
-void lw_func(I* instruction) {
+void lw_func(I *instruction)
+{
 }
-void sw_func(I* instruction) {
+void sw_func(I *instruction)
+{
 }
-void lh_func(I* instruction) {
+void lh_func(I *instruction)
+{
 }
-void sh_func(I* instruction) {
+void sh_func(I *instruction)
+{
 }
--- type_r_func.c
+++ type_r_func.c
@@ -1,58 +1,78 @@
+/* 
+* R type Instructions.
+* Math and Logic Instructions: add, sub, and, or, nor.
+* Copy Instructions: move, mvhi, mvlo.
+* Each Instruction has 3 operands and 3 registers
+* The Instructions is being done between the "rs" and the "rt" registers, and the resualt is being saved in the "rd" register.
+* All of the Instructions have the same code, opcode = 0 (include nothing).
+* Each Instruction has a uniqe name field, the "funct" field, which is uniqe for each Instruction.
+*/
+
 #include "assembler.h"
 #include "param.h"
 #include "func.h"
 
-void add_func(R *instruction) {
+void add_func(R *instruction)
+{
     unsigned int rs = (regArray[instruction->rs]->val);
     unsigned int rt = (regArray[instruction->rt]->val);
     unsigned int rd = rs + rt;
     regArray[instruction->rd]->val = rd;
 }
 
-void sub_func(R *instruction) {
+void sub_func(R *instruction)
+{
     unsigned int rs = (regArray[instruction->rs]->val);
     unsigned int rt = (regArray[instruction->rt]->val);
     unsigned int rd = rs - rt;
     regArray[instruction->rd]->val = rd;
 }
-void and_func(R *instruction) {
+void and_func(R *instruction)
+{
     unsigned int rs = (regArray[instruction->rs]->val);
     unsigned int rt = (regArray[instruction->rt]->val);
     unsigned int rd = rs & rt;
     regArray[instruction->rd]->val = rd;
 }
-void or_func(R *instruction) {
+void or_func(R *instruction)
+{
     unsigned int rs = (regArray[instruction->rs]->val);
     unsigned int rt = (regArray[instruction->rt]->val);
     unsigned int rd = (rs | rt);
     regArray[instruction->rd]->val = rd;
 }
-void nor_func(R *instruction) {
+void nor_func(R *instruction)
+{
     unsigned int rs = (regArray[instruction->rs]->val);
     unsigned int rt = (regArray[instruction->rt]->val);
     unsigned int rd = ~(rs | rt);
     regArray[instruction->rd]->val = rd;
 }
-void move_func(R *instruction) {
+void move_func(R *instruction)
+{
     regArray[instruction->rd]->val = (regArray[instruction->rs]->val);
 }
-void mvhi_func(R *instruction) {
+void mvhi_func(R *instruction)
+{
     /* copy bits 16-31 from rs to rd */
     int i;
     unsigned int rs = (regArray[instruction->rs]->val);
     unsigned int rd = (regArray[instruction->rd]->val);
-    for (i = 31; i > 15; i--) {
+    for (i = 31; i > 15; i--)
+    {
         char bit = CHECK_BIT(rs, i);
         CHANGE_BIT(rd, bit, i);
     }
     regArray[instruction->rd]->val = rd;
 }
-void mvlo_func(R *instruction) {
+void mvlo_func(R *instruction)
+{
     /* copy bits 0-15 from rs to rd */
     int i;
     unsigned int rs = (regArray[instruction->rs]->val);
     unsigned int rd = (regArray[instruction->rd]->val);
-    for (i = 0; i < 16; i++) {
+    for (i = 0; i < 16; i++)
+    {
         char bit = CHECK_BIT(rs, i);
         CHANGE_BIT(rd, bit, i);
     }
